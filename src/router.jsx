cfc98f2c3af7cb5e999421e8ebb0f83c7f773e48
Justin Sheu <justinsheu3341@gmail.com>
2025-02-27 18:06:37 -0800

    Update #2

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function AppRouter() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/work" element={<Work />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </BrowserRouter>
+        <></>
     );
 }
 
